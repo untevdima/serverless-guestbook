91dc65eddbc30636f97209bbc7778d1bce59b0dc
untevdima <97598267+untevdima@users.noreply.github.com>
2022-03-10 12:24:30 +1100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://d2c2d200.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
